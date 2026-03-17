bdfaf851dba92df005966bb81b351dafdb76b3ec
Tom-Sawyer-123 <yungf4st@gmail.com>
2026-03-17 01:30:21 -0700

    change name of class Play to TicTacToe

--- src/main/java/io/github/tommydashed/tictactoe/Main.java
+++ src/main/java/io/github/tommydashed/tictactoe/Main.java
@@ -3,7 +3,7 @@ package io.github.tommydashed.tictactoe;
 import io.github.tommydashed.tictactoe.cli.InputHandler;
 import io.github.tommydashed.tictactoe.cli.OutputHandler;
 import io.github.tommydashed.tictactoe.core.Game;
-import io.github.tommydashed.tictactoe.cli.Play;
+import io.github.tommydashed.tictactoe.cli.TicTacToe;
 public class Main {
 
 
@@ -11,7 +11,7 @@ public class Main {
         Game game = new Game();
         OutputHandler output = new OutputHandler();
         InputHandler input = new InputHandler();
-        Play play = new Play(game, output, input);
+        TicTacToe play = new TicTacToe(game, output, input);
         play.start();
 
     }
--- src/main/java/io/github/tommydashed/tictactoe/cli/TicTacToe.java
+++ src/main/java/io/github/tommydashed/tictactoe/cli/TicTacToe.java
@@ -1,12 +1,12 @@
 package io.github.tommydashed.tictactoe.cli;
 import io.github.tommydashed.tictactoe.core.Game;
 
-public class Play {
+public class TicTacToe {
     private final Game game;
     private final OutputHandler output;
     private final InputHandler input;
 
-    public Play(Game game, OutputHandler output, InputHandler input) {
+    public TicTacToe(Game game, OutputHandler output, InputHandler input) {
         this.game = game;
         this.output = output;
         this.input = input;
